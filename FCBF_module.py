b99802666a59ff2d63d9011a7a052ba398a3b4cf
Santiago Egea Gómez <santiago.egea.gomez@gmail.com>
2017-05-11 11:40:19 +0200

    FCBF_module version 1

---
+++ FCBF_module.py
@@ -0,0 +1,325 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def count_vals(x):
+    vals = np.unique(x)
+    occ = np.zeros(shape = vals.shape)    
+    for i in range(vals.size):
+        occ[i] = np.sum(x == vals[i])
+    return occ
+
+def entropy(x):
+    n = float(x.shape[0])
+    ocurrence = count_vals(x)
+    px = ocurrence / n
+    return -1* np.sum(px*np.log2(px))
+
+def symmetricalUncertain(x,y):
+    n = float(y.shape[0])
+    vals = np.unique(y)
+    # Computing Entropy for the feature x. 
+    Hx = entropy(x)
+    # Computing Entropy for the feature y.
+    Hy = entropy(y)
+    #Computing Joint entropy between x and y.
+    partial = np.zeros(shape = (vals.shape[0]))
+    for i in range(vals.shape[0]):    
+       partial[i] = entropy(x[y == vals[i]]) 
+    
+    partial[np.isnan(partial)==1] = 0      
+    py = count_vals(y).astype(dtype = 'float64') / n
+    Hxy = np.sum(py[py > 0]*partial) 
+    IG = Hx-Hxy
+    return 2*IG/(Hx+Hy)
+
+def suGroup(x, n):
+    m = x.shape[0]
+    x = np.reshape(x, (n,m/float(n))).T
+    m = x.shape[1]
+    SU_matrix = np.zeros(shape = (m,m))
+    for j in range(m-1):
+        x2 = x[:,j+1::]
+        y = x[:,j]
+        temp = np.apply_along_axis(symmetricalUncertain, 0, x2, y)
+        for k in range(temp.shape[0]):
+            SU_matrix[j,j+1::] = temp
+            SU_matrix[j+1::,j] = temp
+    
+    return 1/float(m-1)*np.sum(SU_matrix, axis = 1)
+
+def isprime(a):
+    return all(a % i for i in xrange(2, a))
+
+def get_i(a):
+    if isprime(a):
+        a -= 1
+    return filter(lambda x: a % x == 0, range(2,a))
+
+
+    
+
+class FCBF:
+    
+    idx_sel = []
+    
+    
+    def __init__(self, th = 0.01):
+        '''
+        Parameters
+        ---------------
+            th = The initial threshold 
+        '''
+        self.th = th
+
+
+    def fit(self, x, y):
+        '''
+        This function executes FCBF algorithm and saves indexes 
+        of selected features in self.idx_sel
+        
+        Parameters
+        ---------------
+            x = dataset  [NxM] 
+            y = label    [Nx1]
+        '''
+        self.idx_sel = []
+        """
+        First Stage: Computing the SU for each feature with the response.
+        """
+        SU_vec = np.apply_along_axis(symmetricalUncertain, 0, x, y)
+        SU_list = SU_vec[SU_vec > self.th]
+        SU_list[::-1].sort()
+        
+        m = x[:,SU_vec > self.th].shape
+        x_sorted = np.zeros(shape = m)
+        
+        for i in range(m[1]):
+            ind = np.argmax(SU_vec)
+            SU_vec[ind] = 0
+            x_sorted[:,i] = x[:,ind].copy()
+            self.idx_sel.append(ind)
+        
+        """
+        Second Stage: Identify relationships between feature to remove redundancy.
+        """
+        j = 0
+        while True:
+            """
+            Stopping Criteria:The search finishes
+            """
+            if j >= x_sorted.shape[1]: break
+            y = x_sorted[:,j].copy()
+            x_list = x_sorted[:,j+1:].copy()
+            if x_list.shape[1] == 0: break
+                
+                
+            SU_list_2 = SU_list[j+1:]
+            SU_x = np.apply_along_axis(symmetricalUncertain, 0, 
+                                       x_list, y)
+            
+            comp_SU = SU_x >= SU_list_2
+            to_remove = np.where(comp_SU)[0] + j + 1 
+            if to_remove.size > 0:
+                x_sorted = np.delete(x_sorted, to_remove, axis = 1)
+                SU_list = np.delete(SU_list, to_remove, axis = 0)
+                to_remove.sort()
+                for r in reversed(to_remove): 
+                    self.idx_sel.remove(self.idx_sel[r])
+            j = j + 1        
+            
+    def fit_transform(self, x, y):
+        '''
+        This function fits the feature selection 
+        algorithm and returns the resulting subset.
+        
+        Parameters
+        ---------------
+            x = dataset  [NxM] 
+            y = label    [Nx1]
+        '''
+        self.fit(x, y)
+        return x[:,self.idx_sel]
+         
+    def transform(self, x):
+        '''
+        This function applies the selection
+        to the vector x.
+        
+        Parameters
+        ---------------
+            x = dataset  [NxM] 
+        '''
+        return x[:, self.idx_sel]  
+
+
+
+class FCBFK(FCBF):
+    
+    idx_sel = []
+    
+    
+    def __init__(self, k = 10):
+        '''
+        Parameters
+        ---------------
+            k = Number of features to include in the
+            subset.
+        '''
+        self.k = k
+
+
+    def fit(self, x, y):
+        '''
+        This function executes FCBFK algorithm and saves indexes 
+        of selected features in self.idx_sel
+        
+        Parameters
+        ---------------
+            x = dataset  [NxM] 
+            y = label    [Nx1]
+        '''        
+        self.idx_sel = []
+        """
+        First Stage: Computing the SU for each feature with the response.
+        """    
+        SU_vec = np.apply_along_axis(symmetricalUncertain, 0, x, y)
+        
+        SU_list = SU_vec[SU_vec > 0]
+        SU_list[::-1].sort()
+        
+        m = x[:,SU_vec > 0].shape
+        x_sorted = np.zeros(shape = m)
+        
+        for i in range(m[1]):
+            ind = np.argmax(SU_vec)
+            SU_vec[ind] = 0
+            x_sorted[:,i] = x[:,ind].copy() 
+            self.idx_sel.append(ind)
+            
+        """
+        Second Stage: Identify relationships between features to remove redundancy with stopping 
+        criteria (features in x_best == k).
+        """    
+        j = 0
+        while True:
+            y = x_sorted[:,j].copy()
+            SU_list_2 = SU_list[j+1:]    
+            x_list = x_sorted[:,j+1:].copy()
+            
+            """
+            Stopping Criteria:The search finishes
+            """
+            if x_list.shape[1] == 0: break
+                
+                
+            SU_x = np.apply_along_axis(symmetricalUncertain, 0, 
+                                       x_list, y)
+            
+            comp_SU = SU_x >= SU_list_2
+            to_remove = np.where(comp_SU)[0] + j + 1 
+            if to_remove.size > 0 and x.shape[1] > self.k:
+    
+                for i in reversed(to_remove):
+                    
+                    x_sorted = np.delete(x_sorted, i, axis = 1)
+                    SU_list = np.delete(SU_list, i, axis = 0)                
+                    self.idx_sel.remove(self.idx_sel[i])
+                    if x_sorted.shape[1] == self.k: break  
+                    
+            if x_list.shape[1] == 1 or x_sorted.shape[1] == self.k: break    
+            j = j + 1
+    
+class FCBFiP(FCBF):
+    
+    idx_sel = []
+
+
+    def __init__(self, k = 10, npieces = 2):
+        '''
+        Parameters
+        ---------------
+            k = Number of features to include in the
+            subset.
+            npieces = Number of pieces to divide the 
+            feature space.
+        '''
+        self.k = k
+        self.npieces = npieces
+
+    def fit(self, x, y):
+        '''
+        This function executes FCBF algorithm and saves indexes 
+        of selected features in self.idx_sel
+        
+        Parameters
+        ---------------
+            x = dataset  [NxM] 
+            y = label    [Nx1]
+        '''
+    
+        """
+        First Stage: Computing the SU for each feature with the response. We sort the 
+        features. When we have a prime number of features we remove the last one from the
+        sorted features list.
+        """      
+        m = x.shape
+        nfeaturesPieces = int(m[1] / float(self.npieces))        
+        SU_vec = np.apply_along_axis(symmetricalUncertain, 0, x, y)
+        
+        x_sorted = np.zeros(shape = m, dtype = 'float64')        
+        idx_sorted = np.zeros(shape = m[1], dtype = 'int64')
+        for i in range(m[1]):
+            ind = np.argmax(SU_vec)
+            SU_vec[ind] = -1
+            idx_sorted[i]= ind 
+            x_sorted[:,i] = x[:,ind].copy() 
+
+        if isprime(m[1]): 
+            x_sorted = np.delete(x_sorted, m[1]-1, axis = 1 )
+            ind_prime = idx_sorted[m[1]-1]
+            idx_sorted = np.delete(idx_sorted, m[1]-1)
+            #m = x_sorted.shape
+        """
+        Second Stage: Identify relationships between features into its vecinity
+        to remove redundancy with stopping criteria (features in x_best == k).
+        """   
+        
+        x_2d = np.reshape(x_sorted.T, (self.npieces, nfeaturesPieces*m[0])).T
+    
+        SU_x =  np.apply_along_axis(suGroup, 0, x_2d, nfeaturesPieces)
+        SU_x = np.reshape(SU_x.T, (self.npieces*nfeaturesPieces,))
+        idx_sorted2 = np.zeros(shape = idx_sorted.shape, dtype = 'int64')
+        SU_x[np.isnan(SU_x)] = 1        
+        
+        for i in range(idx_sorted.shape[0]):
+            ind =  np.argmin(SU_x)
+            idx_sorted2[i] = idx_sorted[ind]
+            SU_x[ind] = 10    
+        
+        """
+        Scoring step
+        """        
+        self.scores = np.zeros(shape = m[1], dtype = 'int64') 
+           
+        for i in range(m[1]):
+            if i in idx_sorted:
+                self.scores[i] = np.argwhere(i == idx_sorted) + np.argwhere(i == idx_sorted2)
+        if isprime(m[1]): 
+            self.scores[ind_prime] = 2*m[1]
+        self.set_k(self.k)
+
+
+    def set_k(self, k):
+        self.k = k 
+        scores_temp = -1*self.scores
+        
+        self.idx_sel = np.zeros(shape = self.k, dtype = 'int64')
+        for i in range(self.k):
+            ind =  np.argmax(scores_temp)
+            scores_temp[ind] = -100000000
+            self.idx_sel[i] = ind
+#        self.idx_sel = np.sort(self.idx_sel)       
+        
+        
+
+        
